10f069a3ec3d4ec8dd320414c74c04f1ecd1b00a
erlandev <erlanklink@gmail.com>
2022-07-10 11:16:36 +0600

    key

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -13,7 +13,7 @@ const Gallery = () => {
     getData()
   }, [])
   const getData = async () => {
-    const { data } = await axios.get("https://api.flickr.com/services/rest?primary_photo_extras=owner_name%2Cpath_alias%2Crealname%2Csizes%2Curl_sq%2Curl_q%2Curl_t%2Curl_s%2Curl_n%2Curl_w%2Curl_m%2Curl_z%2Curl_c%2Curl_l%2Curl_h%2Curl_k%2Curl_3k%2Curl_4k%2Curl_5k%2Curl_6k%2Cneeds_interstitial&page=1&per_page=25&get_user_info=1&extras=can_share%2Ccan_download&path_alias=manchodevs&viewerNSID=&method=flickr.photosets.getList&csrf=&api_key=dadbe1a6ad1255361246a6b49382d615&format=json&hermes=1&hermesClient=1&reqId=f6707bfe-a0f0-4759-9ce2-3b9590d81ce3&nojsoncallback=1")
+    const { data } = await axios.get("https://api.flickr.com/services/rest?primary_photo_extras=owner_name%2Cpath_alias%2Crealname%2Csizes%2Curl_sq%2Curl_q%2Curl_t%2Curl_s%2Curl_n%2Curl_w%2Curl_m%2Curl_z%2Curl_c%2Curl_l%2Curl_h%2Curl_k%2Curl_3k%2Curl_4k%2Curl_5k%2Curl_6k%2Cneeds_interstitial&per_page=25&get_user_info=1&extras=can_share%2Ccan_download&user_id=193653472%40N05&viewerNSID=&method=flickr.photosets.getList&csrf=&api_key=b5bfddabcc7dbda91f8d5111bca73b30&format=json&hermes=1&hermesClient=1&reqId=65d194d3-90a7-45c8-be4e-22be2efbaef4&nojsoncallback=1")
     setDataAlbums(data.photosets);
     setActive(true);
   }
